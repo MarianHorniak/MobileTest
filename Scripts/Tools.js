60548609eda379cf4dd40717098091458c58dbb7
MarianHorniak <marian.horniak@gmail.com>
2016-11-08 14:28:49 +0100

    rev 1

---
+++ Scripts/Tools.js
@@ -0,0 +1,24 @@
+﻿
+var Logs = "";
+
+
+function Log(message)
+{
+    console.log(message);
+}
+
+
+function Alert(message)
+{
+    alert(message);
+}
+
+
+function startTimer()
+{
+    window.setTimeout(doTimeout, 10000);
+}
+
+function doTimeout() {
+    Alert('Fired');
+}
