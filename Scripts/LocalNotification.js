e50a0f99fbebe97803cd27fb9a3706184d1b9402
pko3 <peter.kotry@gmail.com>
2017-04-26 18:57:58 +0200

    popis

--- Scripts/LocalNotification.js
+++ Scripts/LocalNotification.js
@@ -1,5 +1,4 @@
-﻿//https://github.com/katzer/cordova-plugin-local-notifications/wiki/11.-Samples
-//https://github.com/evothings/phonegap-estimotebeacons/wiki/Background-notification-guide
+﻿//https://github.com/katzer/cordova-plugin-local-notifications/blob/9a13f4e/README.md
 
 var LocalNotification = {
     allowSedule: false,
@@ -33,19 +32,24 @@ var LocalNotification = {
                     
                 });
 
-                //cordova.plugins.notification.local.getScheduledIds(function (scheduledIds) {
-                //    app.log("scheduledIds: " + scheduledIds);
-                //    if (scheduledIds && scheduledIds.indexOf(id) != -1) {
-                //        cordova.plugins.notification.local.update({ id: id, text: text });
-                //        app.log("LocalNotification.update: id:" + id + " text:" + text);
-                //    }
-                //    else {
-                //        cordova.plugins.notification.local.add({ id: id, text: text });
-                //        app.log("cordova.plugins.notification.local.add id:" + id + " text:" + text);
-                //    }
-                //})
+                /*
+                window.plugin.notification.local.add({
+                    id:         String,  // A unique id of the notifiction
+                    date:       Date,    // This expects a date object
+                    message:    String,  // The message that is displayed
+                    title:      String,  // The title of the message
+                    repeat:     String,  // Either 'secondly', 'minutely', 'hourly', 'daily', 'weekly', 'monthly' or 'yearly'
+                    badge:      Number,  // Displays number badge to notification
+                    sound:      String,  // A sound to be played
+                    json:       String,  // Data to be passed through the notification
+                    autoCancel: Boolean, // Setting this flag and the notification is automatically canceled when the user clicks it
+                    ongoing:    Boolean, // Prevent clearing of notification (Android only)
+                });
+                
+                */
+
+               
             } catch (err) {
-                    //this.allowSedule = false;
                 app.log("cordova.plugins.notification.local.isScheduled: " + err);
                     return;
                 }
